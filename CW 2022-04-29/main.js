eebec20c8a8ca76ebe1ee9ad59618019158e7a2e
nzclimbrr <98310189+nzclimbrr@users.noreply.github.com>
2022-04-29 15:48:05 +1200

    Delete main.js

--- CW 2022-04-29/main.js
+++
@@ -1,5 +0,0 @@
-// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
-
-function evenOrOdd(number) {
-  return (number % 2 === 0 ? "Even" : "Odd")
-}
