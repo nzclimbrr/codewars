1933314adaf43f163bd22f80588c99958f453092
nzclimbrr <sapowen@gmail.com>
2022-05-01 16:18:35 +1200

    Coding challenge and solution for 01-05-22 coding challenge.

--- Grasshopper - Messi Goals/main.js
+++ Grasshopper - Messi Goals/main.js
@@ -16,4 +16,8 @@
 // copaDelReyGoals
 // Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
 
+const laLigaGoals = 43
+const championsLeagueGoals = 10
+const copaDelReyGoals = 5
 
+let totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
