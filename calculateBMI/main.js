f72de61f7a1ffa8a5ded768eec8148b6cafdda5a
nzclimbrr <sapowen@gmail.com>
2022-05-03 14:31:06 +1200

    Coding challenge and solution for 03-05-22 coding challenge.

--- calculateBMI/main.js
+++ calculateBMI/main.js
@@ -9,5 +9,14 @@
 // if bmi > 30 return "Obese"
 
 function bmi(weight, height) {
-  return "";
+let bmi = weight / (height ** 2)
+  if (bmi <= 18.5) {
+    return "Underweight"
+  } else if (bmi <= 25.0) {
+    return "Normal"
+  } else if (bmi <= 30.0) {
+    return "Overweight"
+  } else if (bmi > 30) {
+    return "Obese"
+  }
 }
